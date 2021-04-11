3939961a5ed8d09c64d99d983dc2d2883cd00354
Leon Lin <qqqq12368@gmail.com>
2021-04-11 22:04:56 +0800

    final

--- password_retry.py
+++ password_retry.py
@@ -7,11 +7,15 @@
 password = 'a123456' #正解
 b = 3 #可輸入密碼次數
 while b > 0:
+	b = b - 1
 	a = input('請輸入密碼')
 	if a == password:
 		print('登入成功')
 		break
 	else: 
-		b = b - 1
-		print('密碼錯次', '還有', b, '次機會')
+		print('密碼錯誤')
+		if b > 0:
+			print('還有', b, '次機會')
+		else:
+			print('沒機會嘗試了')
 		
